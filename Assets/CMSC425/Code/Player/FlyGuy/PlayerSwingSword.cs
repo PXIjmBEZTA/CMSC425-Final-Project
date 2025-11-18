112646fd96557b96e7b6d7bd6bca97dddba3aedd
PXIjmBEZTA <aakbar1@terpmail.umd.edu>
2025-11-18 00:38:46 -0500

    Some sword things fixed
    Sword spins 360 around player.
    Has a cool-ish trail.
    Doesn't do anything functional yet.

---
+++ Assets/CMSC425/Code/Player/FlyGuy/PlayerSwingSword.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class PlayerSwingSword : MonoBehaviour
+{
+    [Header("Sword Settings")]
+    public GameObject sword;
+    public float swingDuration = 0.4f;
+    public float swingCooldown = 1f;
+    public float swordDistance = 1;
+
+    [Header("Controls")]
+    public ButtonControl swordButton;
+
+
+    private bool isSwinging = false;
+    private float nextSwingTime = 0f;
+    private Quaternion startRotation;
+    private TrailRenderer trail;
+    void Start()
+    {
+        startRotation = sword.transform.localRotation;
+        swordButton = Mouse.current.rightButton;
+        if (sword == null)
+            Debug.LogWarning("Sword is not assigned! - PlayerSwingSword");
+        sword.transform.localPosition = new Vector3(0, 0, swordDistance);
+        sword.transform.localRotation = Quaternion.identity;
+        sword.SetActive(false);
+
+        trail = sword.GetComponent<TrailRenderer>();
+        if (trail)
+        {
+            trail.enabled = false; //initially disabled
+            trail.startWidth = 0.3f;
+            trail.endWidth = 0;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (swordButton == null) return;
+
+        if (swordButton.isPressed && !isSwinging && Time.time >= nextSwingTime)
+        {
+            StartCoroutine(SwingSword());
+        }
+    }
+
+    private IEnumerator SwingSword()
+    {
+        isSwinging = true;
+        nextSwingTime = Time.time + swingCooldown;
+        sword.SetActive(true);
+
+        trail.Clear();
+        trail.enabled = true;
+
+        float elapsed = 0f;
+        while (elapsed < swingDuration)
+        {
+            elapsed += Time.deltaTime;
+            float anglePerSecond = 360 / swingDuration;
+
+            sword.transform.RotateAround(transform.position, Vector3.up, anglePerSecond * Time.deltaTime);
+            yield return null;
+        }
+
+        trail.enabled = false;
+        sword.transform.localPosition = new Vector3(0, 0, swordDistance);
+        sword.transform.localRotation = startRotation;
+        sword.SetActive(false);
+        isSwinging = false;
+    }
+}
